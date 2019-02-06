6261a4273bfff744c5dffcb6f065c253a71ea5d1
Francisco Javier Luna Luis <fcojlunaluis@gmail.com>
2019-02-06 08:51:38 -0600

    Agrega proyetoo de calculadora con sus pruebas unitarias

---
+++ TestCalculadora.py
@@ -0,0 +1,13 @@
+from unittest import TestCase
+from Calculadora import Calculadora
+
+class TestCalculadora(TestCase):
+
+	def test_suma_2_mas_2(self)
+		calc = Calculadora()
+		self.assertEquals(4, calc.suma(2,2))
+
+	if __name__ == '__main__':
+		unittest.main()
+
+
