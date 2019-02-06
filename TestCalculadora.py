dd61375c6e292bdc3e814b99f78af2b4544f644a
Francisco Javier Luna Luis <fcojlunaluis@gmail.com>
2019-02-06 09:13:00 -0600

    Modifica prueba

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 from Calculadora import Calculadora
 
@@ -10,7 +11,7 @@ class TestCalculadora(TestCase):
 	def test_suma_5_mas_10(self)
 		calc = Calculadora()
 		self.assertEquals(15, calc.suma(5,10))
-		
+
 
 	if __name__ == '__main__':
 		unittest.main()
