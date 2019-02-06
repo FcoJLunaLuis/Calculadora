98016b514e9fef056bcf5b805473769d3f5c82b6
Francisco Javier Luna Luis <fcojlunaluis@gmail.com>
2019-02-06 09:05:04 -0600

    Agrega prueba de 5 mas 10

--- TestCalculadora.py
+++ TestCalculadora.py
@@ -7,6 +7,11 @@ class TestCalculadora(TestCase):
 		calc = Calculadora()
 		self.assertEquals(4, calc.suma(2,2))
 
+	def test_suma_5_mas_10(self)
+		calc = Calculadora()
+		self.assertEquals(15, calc.suma(5,10))
+		
+
 	if __name__ == '__main__':
 		unittest.main()
 
